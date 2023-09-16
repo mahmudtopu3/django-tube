ef9dca34497d8e0043ddbe58cb78827f94f97310
mahmudtopu3 <mahmudtopu3@gmail.com>
2023-09-16 16:35:00 +0600

    updated encoding code as well generate thumbnails

--- video/management/commands/encode.py
+++ video/management/commands/encode.py
@@ -10,16 +10,18 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         try:
-            obj = Video.objects.filter(status='Pending').first()
+            #obj = Video.objects.filter(status='Pending').first()
             obj = Video.objects.first()
             if obj:
                 obj.status = 'Processing'
+                obj.is_running = True
                 obj.save()
                 input_video_path = obj.video.path
                 output_directory = os.path.join(os.path.dirname(input_video_path), 'hls_output')
                 os.makedirs(output_directory, exist_ok=True)
                 output_filename = os.path.splitext(os.path.basename(input_video_path))[0] + '_hls.m3u8'
                 output_hls_path = os.path.join(output_directory, output_filename)
+                output_thumbnail_path = os.path.join(output_directory, os.path.splitext(os.path.basename(input_video_path))[0]+'thumbnail.jpg')
 
                 # getting video duration/length
 
@@ -28,6 +30,7 @@ class Command(BaseCommand):
                     "-v", "quiet",
                     "-print_format", "json",
                     "-show_streams",
+                    
                     input_video_path
                 ]
                 result = subprocess.run(command, shell=False,
@@ -54,15 +57,32 @@ class Command(BaseCommand):
                     '-hls_list_size', '0',
                     "-hls_base_url", "{{ dynamic_path }}/",
                     "-movflags", "+faststart",
+                    '-y',
                     output_hls_path
                 ]
 
 
                 subprocess.run(cmd, check=True)
 
+
+                # generate thumbnail 
+                ffmpeg_cmd = [
+                    'ffmpeg',
+                    '-i', input_video_path,
+                    '-ss', '2', 
+                    '-vframes', '1',            
+                    '-q:v', '2',  
+                    '-y',               
+                    output_thumbnail_path
+                ]
+                subprocess.run(ffmpeg_cmd, check=True)
+
+
                 # Update the Video object status to 'Processed' or something similar
                 obj.hls = output_hls_path 
+                obj.thumbnail = output_thumbnail_path
                 obj.status = 'Completed'
+                obj.is_running = False
                 obj.save()
 
                 print(f'HLS segments generated and saved at: {output_hls_path}')
